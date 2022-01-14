dd95950997d5c5a95246d85a3d84887d9066b6c9
pranay101 <pranayprajapati101@gmail.com>
2022-01-14 20:21:50 +0530

    Implemented Subtotal Component

--- src/Component/Checkout/Subtotal/Subtotal.js
+++ src/Component/Checkout/Subtotal/Subtotal.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import classes from "./Subtotal.module.css"
+import { useStateValue } from '../../ReactContextAPI/StateProvider/StateProvider'
 function Subtotal() {
+
+    const [{basket},dispatch] = useStateValue(); 
+    const [totalPriceState,settotalPriceState] = useState()
+    let totalPrice = 0
+    console.log(basket);
+    basket.forEach(item => {
+        totalPrice = totalPrice + item.price
+    });
+    // totalPrice = basket.map( item =>{ return totalPrice + item.price})
+    // settotalPriceState(totalPrice)
     return (
         <div className={classes.subtotal}>
-            <p>Subtotal (0 items) : <strong>0</strong></p>
+            <p>Subtotal ({basket.length} items) : 
+            <strong> ₹ {totalPrice}</strong></p>
             <small className={classes.subtotal__gift}>
                 <input type={"checkbox"} />
                 This Order Contains a gift.
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -18,7 +18,7 @@ function Home() {
               .toString()
               .substring(1)}
             title=" The Lean Startup: How Constant Innovation Creates Radically Successful Businesses"
-            price={"640"}
+            price={640}
             rating={5}
             image="https://images-eu.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
           />
@@ -27,7 +27,7 @@ function Home() {
               .toString()
               .substring(1)}
             title="Pidilite D-Klog Drain Cleaner Powder (40g x 5N), Removes Clogs, Blockages in Washbasin, Sinks, Pipes within 30 Minutes, No Manual Hassle with Drain spring pipe, Plunger, Drain Stick, Cleaning Hook"
-            price={"100"}
+            price={100}
             rating={4}
             image="https://m.media-amazon.com/images/I/61zwfuoURpL._SL1000_.jpg"
           />
@@ -38,7 +38,7 @@ function Home() {
               .toString()
               .substring(1)}
             title="Fitbit Charge 4 Fitness and Activity Tracker with Built-in GPS, Heart Rate, Sleep & Swim Tracking, One Size (S & L Bands Included) (Black)"
-            price={"7,999"}
+            price={7999}
             rating={5}
             image="https://m.media-amazon.com/images/I/71smqRr0pmL._AC_UY218_.jpg"
           />
@@ -47,7 +47,7 @@ function Home() {
               .toString()
               .substring(1)}
             title="Echo Dot (3rd Gen) - #1 smart speaker brand in India with Alexa (Black)"
-            price={"3,499"}
+            price={3499}
             rating={4}
             image="https://m.media-amazon.com/images/I/61EXU8BuGZL._AC_UY218_.jpg"
           />
@@ -56,7 +56,7 @@ function Home() {
               .toString()
               .substring(1)}
             title="2021 Apple iPad Pro with Apple M1 chip (12.9-inch/32.77 cm, Wi-Fi, 256GB) - Space Grey (5th Generation)"
-            price={"1,04,599"}
+            price={104599}
             rating={5}
             image="https://m.media-amazon.com/images/I/81+N4PFF7jS._AC_UY218_.jpg"
           />
@@ -67,7 +67,7 @@ function Home() {
               .toString()
               .substring(1)}
             title="Samsung LC34J791WTWXXL 34' Wide ScreenThunderbolt 3 Curved Monitor 3440 x 1440"
-            price={"80,000"}
+            price={80000}
             rating={4}
             image="https://m.media-amazon.com/images/I/91-hWcNu2yL._AC_UY218_.jpg"
           />
